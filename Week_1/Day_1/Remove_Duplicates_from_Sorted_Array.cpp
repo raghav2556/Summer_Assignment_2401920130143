1c4e5fca6b8a458cedf503efbed3e41534f5d28d
raghav2556 <raghvendrashah21@gmail.com>
2026-06-01 12:54:29 +0530

    Problem 2

---
+++ Week_1/Day_1/Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class solution {
+    public:
+        int removeduplicates(vector<int> &nums) {
+            int k = 1;
+
+            for (int i=1; i<nums.size(); i++) {
+                if (nums[i] != nums[i-1]) {
+                    nums[k] = nums[i];
+                    k++;
+                }
+            }
+            return k;
+        }
+};
+
