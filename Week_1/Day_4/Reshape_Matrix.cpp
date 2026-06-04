6fd628dc89cb380323dc4950c11e4152f3a61aad
raghav2556 <raghvendrashah21@gmail.com>
2026-06-04 18:43:50 +0530

    problem 11

---
+++ Week_1/Day_4/Reshape_Matrix.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class solution {
+    vector<vector<int>> reshapematrix(vector<vector<int>> &mat , int r , int c) {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        if (m*n != r*c) return mat;
+        vector<vector<int>> ans(r , vector<int>(c));
+
+        for (int i=0; i<m*n; i++) ans[i/c][i%c] = mat[i/m][i%m];
+
+        return ans;
+    }
+};
