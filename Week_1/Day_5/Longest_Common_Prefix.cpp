11ebddc6a7c2fd259922fdd76771c1feaea80d15
raghav2556 <raghvendrashah21@gmail.com>
2026-06-05 16:54:28 +0530

    problem 15

---
+++ Week_1/Day_5/Longest_Common_Prefix.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class solution {
+    public:
+       string prefix(vector<string> &strs) {
+        if (strs.size() == 0) return "";
+
+        string pre = strs[0];
+
+        for (int i=1; i<strs.size(); i++) {
+            while(strs[i].find(pre) != 0) pre = pre.substr(0 , pre.size()-1);
+        }
+        return pre;
+       }
+};
